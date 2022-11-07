8098579ced5d9a8db9c85af6938126a1ae50f606
SaranyaPrakash <saranyaprakash.qa@gmail.com>
2022-11-07 16:15:17 -0500

    Reducing the code using Java Reflection API

--- keywordDrivenLearning/src/test/java/execution/Engine.java
+++ keywordDrivenLearning/src/test/java/execution/Engine.java
@@ -1,33 +1,58 @@
 package execution;
 
 import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 import actionkeywords.ActionKeywords;
 import utilities.ExcelUtilities;
 
 public class Engine {
 
-	public static void main(String[] args) throws IOException {
+	ActionKeywords actionKeyword;
+	Method[] methods;
+	static String  keyword;
+	
+	public void getActionKeywords() {
+		/*ActionKeywords actionKeyword = new ActionKeywords();
+		Method[] methods = actionKeyword.getClass().getMethods();*/
+		
+		actionKeyword = new ActionKeywords();
+		methods = actionKeyword.getClass().getMethods();
+		
+	}
+	
+	public void executeKeywords() throws Exception {
+		for(int i = 0; i< methods.length; i++) {
+			if(methods[i].getName().equalsIgnoreCase(keyword)) {
+				methods[i].invoke(actionKeyword);
+			}
+		}
+		
+	}
+	
+	public static void main(String[] args) throws Exception {
 		
 		ExcelUtilities utility = new ExcelUtilities();
 		utility.readExcelFile("src\\test\\resources\\keywordDriven.xlsx");
 		
+		Engine engine = new Engine();
+		
+		engine.getActionKeywords();
 		
 		for(int row = 1; row<=5; row++) {
-		String keyword = 
-				utility.getCellContentFromExcel(row, 3);
+			keyword = utility.getCellContentFromExcel(row, 3);
+			engine.executeKeywords();
 		
-		if(keyword.equalsIgnoreCase("openBrowser")) {
-			ActionKeywords.openBrowser();
-		}else if (keyword.equalsIgnoreCase("goToURL")) {
-			ActionKeywords.goToURL();
-		}else if (keyword.equalsIgnoreCase("enterUsername")) {
-			ActionKeywords.enterUsername();
-		}else if (keyword.equalsIgnoreCase("enterPassword")) {
-			ActionKeywords.enterPassword();
-	}else if (keyword.equalsIgnoreCase("clickLogin")) {
-		ActionKeywords.clickLogin();
-}
+		/*
+		 * if(keyword.equalsIgnoreCase("openBrowser")) { ActionKeywords.openBrowser();
+		 * }else if (keyword.equalsIgnoreCase("goToURL")) { ActionKeywords.goToURL();
+		 * }else if (keyword.equalsIgnoreCase("enterUsername")) {
+		 * ActionKeywords.enterUsername(); }else if
+		 * (keyword.equalsIgnoreCase("enterPassword")) { ActionKeywords.enterPassword();
+		 * }else if (keyword.equalsIgnoreCase("clickLogin")) {
+		 * ActionKeywords.clickLogin(); }
+		 */
 }
 	}
 }
